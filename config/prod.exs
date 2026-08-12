e691c5854118ae4629c440134ac15fbec520a52f
Ammad.Khan <ammadkhan@msn.com>
2026-08-12 16:53:16 +0200

    Enable production SSL enforcement

--- config/prod.exs
+++ config/prod.exs
@@ -3,3 +3,5 @@ import Config
 config :kids_prep, KidsPrep.Repo,
   stacktrace: false,
   show_sensitive_data_on_connection_error: false
+
+config :kids_prep, KidsPrepWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
